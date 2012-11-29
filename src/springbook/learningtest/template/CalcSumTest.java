5989b9004db8686eb430c414a5840e6e4d6c8363
B-Fron <gandamiro@gmail.com>
2012-11-29 22:45:37 +0900

    중복의 제거와 템플릿/콜백 설계

--- src/springbook/learningtest/template/CalcSumTest.java
+++ src/springbook/learningtest/template/CalcSumTest.java
@@ -2,16 +2,24 @@ package springbook.learningtest.template;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 public class CalcSumTest {
-	@Test
-	public void  sumOfNumbers() throws IOException {
-		Calculator calculator = new Calculator();
-		int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
-		assertThat(sum, is(10));
+	Calculator calculator;
+	String numFilepath;
+	
+	@Before public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilepath = getClass().getResource("numbers.txt").getPath();
 	}
-
+	
+	@Test public void sumOfNumbers() throws IOException {
+		assertThat(calculator.calcSum(this.numFilepath), is(10));
+	}
+	@Test public void multiplayOfNumbers() throws IOException {
+		assertThat(calculator.calcMultiply(this.numFilepath), is(24));
+	}
 }
